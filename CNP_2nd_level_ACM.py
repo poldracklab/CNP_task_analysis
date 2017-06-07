69e37730fc112f5878bf79ebeec160fa074ec852
jokedurnez <joke.durnez@gmail.com>
2017-06-07 11:39:30 -0700

    more efficient calculation of ACM's

--- CNP_2nd_level_ACM.py
+++ CNP_2nd_level_ACM.py
@@ -1,11 +1,15 @@
 import nibabel as nib
 import numpy as np
-import os
 import argparse
-from nipype.interfaces import fsl
-from nipype.pipeline.engine import Workflow, Node, MapNode
 import shutil
 import json
+import os
+
+parser = argparse.ArgumentParser(description='Perform analysis on CNP task data')
+parser.add_argument('-task','--task',dest='task',help='task name',required=True)
+args = parser.parse_args()
+
+task = args.task
 
 featdir = os.environ.get("RESUDIR")
 outdir = os.environ.get("GROUPDIR")
@@ -17,41 +21,60 @@ contrastfile = os.path.join(homedir,"utils/contrasts.json")
 with open(contrastfile) as fl:
     contrasts = json.load(fl)
 
-for task in contrasts.keys():
-#for task in ['bart']
+acmtaskdir = os.path.join(acmdir,task)
+if not os.path.exists(acmtaskdir):
+    os.mkdir(acmtaskdir)
 
-    acmtaskdir = os.path.join(acmdir,task)
-    if not os.path.exists(acmtaskdir):
-        os.mkdir(acmtaskdir)
+groupmaskfile = os.path.join(os.path.join(outdir,task),"mask.nii.gz")
+groupmask = nib.load(groupmaskfile).get_data()
 
-    groupmaskfile = os.path.join(os.path.join(outdir,task),"mask.nii.gz")
+for contrast in range(len(contrasts[task])):
 
-    for contrast in range(len(contrasts[task])):
+    ##############################
+    ## COLLECT FEAT DIRECTORIES ##
+    ##############################
 
-        ##############################
-        ## COLLECT FEAT DIRECTORIES ##
-        ##############################
+    featdirs = []
+    for sub in os.listdir(featdir):
+        if sub[:4]!= 'sub-':
+            continue
+        subfeatdir = os.path.join(featdir,sub,"%s.feat"%task)
+        if os.path.exists(subfeatdir):
+            featdirs.append(subfeatdir)
 
-        featdirs = []
-        for sub in os.listdir(featdir):
-            if sub[:4]!= 'sub-':
-                continue
-            subfeatdir = os.path.join(featdir,sub,"%s.feat"%task)
-            if os.path.exists(subfeatdir):
-                featdirs.append(subfeatdir)
+    ################
+    ## create ACM ##
+    ################
 
-        ################
-        ## create ACM ##
-        ################
+    zstats = [os.path.join(x,'stats','%s%i.nii.gz'%('zstat',contrast+1)) for x in featdirs]
 
-        zstats = [os.path.join(x,'stats','%s%i.nii.gz'%('zstat',contrast+1)) for x in featdirs]
+    ACM_pos = np.zeros([65,77,49,len(zstats)])
+    ACM_neg = np.zeros([65,77,49,len(zstats)])
+    for idx,zstat in enumerate(zstats):
+        zmap = nib.load(zstat).get_data()
+        exceed = np.where(zmap>1.65)
+        ACM_pos[exceed[0],exceed[1],exceed[2],idx] = 1
+        exceed = np.where(zmap<-1.65)
+        ACM_neg[exceed[0],exceed[1],exceed[2],idx] = 1
 
-        ACM = np.zeros([65,77,49,len(zstats)])
-        for idx,zstat in enumerate(zstats):
-            zmap = nib.load(zstat).get_data()
-            exceed = np.where(zmap>1.65)
-            ACM[exceed[0],exceed[1],exceed[2],idx] = 1
+    ACM_pos = np.mean(ACM_pos,axis=3)
+    ACM_pos = ACM_pos*groupmask
+    ACM_neg = np.mean(ACM_neg,axis=3)
+    ACM_neg = ACM_neg*groupmask
 
-        ACM = np.mean(ACM,axis=3)
+    ACM_diff = ACM_pos-ACM_neg
 
-        ACMfile = os.path.join(acmtaskdir,"zstat%i_ACM.nii.gz"%(contrast+1))
+    zmap = nib.load(zstats[0])
+    ACMfile = os.path.join(acmtaskdir,"zstat%i_ACM_pos.nii.gz"%(contrast+1))
+    ACMimg = nib.Nifti1Image(ACM_pos,affine=zmap.get_affine(),header=zmap.get_header())
+    ACMimg.to_filename(ACMfile)
+
+    ACMfile = os.path.join(acmtaskdir,"zstat%i_ACM_neg.nii.gz"%(contrast+1))
+    ACMimg = nib.Nifti1Image(ACM_neg,affine=zmap.get_affine(),header=zmap.get_header())
+    ACMimg.to_filename(ACMfile)
+
+    ACMfile = os.path.join(acmtaskdir,"zstat%i_ACM_diff.nii.gz"%(contrast+1))
+    ACMimg = nib.Nifti1Image(ACM_diff,affine=zmap.get_affine(),header=zmap.get_header())
+    ACMimg.to_filename(ACMfile)
+
+    print("generated ACM for task %s, contrast %i"%(task,contrast))
