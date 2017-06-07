b9443e84a3f3f685ae4a6de22bbbb6d8192ce2bf
jokedurnez <joke.durnez@gmail.com>
2017-06-07 11:40:56 -0700

    run all ACM's in one job

--- hpc/04_CNP_2nd_level_ACM.sh
+++ hpc/04_CNP_2nd_level_ACM.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
-#SBATCH --time=30:00:00
-#SBATCH --mem=16GB
-#SBATCH --output=logs/CNP.group.%a.txt
-#SBATCH --error=logs/CNP.group.%a.txt
+#SBATCH --time=5:00:00
+#SBATCH --mem=64GB
+#SBATCH --output=logs/CNP.ACM.txt
+#SBATCH --error=logs/CNP.ACM.txt
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=joke.durnez@gmail.com
-#SBATCH --cpus-per-task=10
+#SBATCH --cpus-per-task=16
 #SBATCH --ntasks=1
-#SBATCH --qos=russpold
-#SBATCH -p russpold
 
 source /oak/stanford/groups/russpold/data/ds000030_R1.0.3_analysis_0.4.4_code/config.sh
-module laod singularity
+module load singularity
 
 unset PYTHONPATH
 
@@ -25,6 +23,13 @@ cd $HOMEDIR
 
 set -e
 
-singularity exec -B $OAK:$OAK $SINGULARITY python -s $HOMEDIR/CNP_2nd_level_ACM.py
+singularity exec -B $OAK:$OAK $SINGULARITY python -s $HOMEDIR/CNP_2nd_level_ACM.py --task=scap & \
+singularity exec -B $OAK:$OAK $SINGULARITY python -s $HOMEDIR/CNP_2nd_level_ACM.py --task=taskswitch & \
+singularity exec -B $OAK:$OAK $SINGULARITY python -s $HOMEDIR/CNP_2nd_level_ACM.py --task=stopsignal & \
+singularity exec -B $OAK:$OAK $SINGULARITY python -s $HOMEDIR/CNP_2nd_level_ACM.py --task=pamret & \
+singularity exec -B $OAK:$OAK $SINGULARITY python -s $HOMEDIR/CNP_2nd_level_ACM.py --task=bart & \
+
+wait
+
 
 echo "༼ つ ◕_◕ ༽つ CNP modeling pipeline finished"
