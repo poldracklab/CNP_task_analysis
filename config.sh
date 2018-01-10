1fcbb0d33b5d7a2b67eba6cd7f1da22d67561731
jokedurnez <joke.durnez@gmail.com>
2018-01-10 11:53:40 -0800

    improve config

--- config.sh
+++ config.sh
@@ -4,12 +4,12 @@ module load singularity/2.3
 ## INPUT
 
 # location of the BIDS dataset
-export BIDSDIR=/oak/stanford/groups/russpold/data/ds000030_R1.0.3/
+export BIDSDIR=/oak/stanford/groups/russpold/data/ds000030/1.0.3/
 # location of the preprocessed data
-export PREPBASEDIR=/oak/stanford/groups/russpold/data/ds000030_R1.0.3_preprocessed_0.4.4/
+export PREPBASEDIR=/oak/stanford/groups/russpold/data/ds000030/1.0.3/derivatives/
 
 # location of this repository :-)
-export HOMEDIR=/oak/stanford/groups/russpold/data/ds000030_R1.0.3_analysis_0.4.4_code/
+export HOMEDIR=$HOME/CNP_analysis/
 
 ## SINGULARITY CONTAINER
 export SINGULARITY=$SCRATCH/poldracklab_cnp_task_analysis-2017-06-13-8a9da76ec6c3.img
