248f62d295429d9cb17fa5e7b416871fba309ce3
Oscar Esteban <code@oscaresteban.es>
2018-04-11 08:25:52 -0700

    refactor 2nd level analysis

--- CNP_2nd_level.py
+++ CNP_2nd_level.py
@@ -1,200 +1,211 @@
-from nipype.pipeline.engine import Workflow, Node
-from nipype.interfaces import fsl
-from utils import get_config
-import nibabel as nib
-import numpy as np
-import argparse
-import shutil
 import os
+import sys
+import inspect
+import numpy as np
+from nipype.interfaces import fsl
+from nipype.interfaces import io as nio
+from nipype.interfaces.utility import Function
+from nipype.pipeline.engine import Workflow, Node
 
-parser = argparse.ArgumentParser(description='Perform analysis on CNP task data')
-parser.add_argument('-task', '--task', dest='task', help='task name', required=True)
-parser.add_argument('-contrast', '--contrast', dest='contrast',
-                    help='contrast number', required=True)
-parser.add_argument('--experiment', '--experiment', dest='experiment',
-                    help='experiment number', required=True)
-args = parser.parse_args()
+GROUP_MASK = ('/oak/stanford/groups/russpold/data/'
+              'templates/mni_icbm152_nlin_asym_09c/2mm_brainmask.nii.gz')
 
-experiment = int(args.experiment)
 
-##############################
-# TAKE SAMPLE FOR EXPERIMENT #
-##############################
+def get_parser():
+    import argparse
+    parser = argparse.ArgumentParser(description='Perform analysis on CNP task data')
+    parser.add_argument('name', action='store', help='workflow name')
+    parser.add_argument('-t', '--task', action='store', help='task name',
+                        required=True)
+    parser.add_argument('-c', '--contrast', action='store', type=int,
+                        help='contrast number', required=True)
+    parser.add_argument('-e', '--experiment', action='store', type=int,
+                        help='experiment number', required=True)
 
-# collect subjects
+    parser.add_argument('-B', '--bids-dir', action='store',
+                        default=os.environ.get('BIDSDIR'),
+                        help='path to the original BIDS root folder')
+    parser.add_argument('-P', '--deriv-dir', action='store',
+                        default=os.environ.get('PREPBASEDIR'),
+                        help='path to a BIDS-Derivatives folder with '
+                             'preprocessed data')
+    parser.add_argument('-w', '--work-dir', action='store',
+                        default=os.path.join(os.getcwd(), 'work'),
+                        help='work directory')
+    return parser
 
-prepdir = os.environ.get("PREPBASEDIR")
-fmriprepdir = os.path.join(prepdir, 'fmriprep-1.0.3', 'fmriprep')
-subjects = [x for x in os.listdir(fmriprepdir) if x[-4:] != 'html' and x[:4] == 'sub-']
 
-subs = []
-for subject in subjects:
-    image = os.path.join(prepdir, 'fmriprep-1.0.3', 'fmriprep', subject, 'func',
-                         '%s_task-%s_bold_space-MNI152NLin2009cAsym_preproc.nii.gz' % (subject, args.task))
-    if os.path.exists(image):
-        subs.append(subject)
+def thresholding(samplesize, zstat1, cope1, res4d, mask):
+    ##################
+    #  THRESHOLDING ##
+    ##################
+    import os
 
-# 50010 and 10527 have missing conditions
-subs = set(subs) - set(['sub-50010', 'sub-10527'])
-subjects = list(subs)
+    # FDR
+    os.popen('fslmaths %s -ztop zstat1_pval' % zstat1).read()
+    logpcmd = 'fdr -i zstat1_pval -m %s -q 0.05' % mask
 
-start = 10
+    pstat = os.path.abspath('zstat1_pval.nii.gz')
+    thres = 1 - float(os.popen(logpcmd).read().split('\n')[1])
+    threscmd = ('fslmaths zstat1_pval -mul -1 -add 1 -thr %f '
+                '-mas %s zstat1_thresh_vox_fdr_pstat1') % (
+        thres, mask)
+    os.popen(threscmd).read()
+    fdr_thres = os.path.abspath('zstat1_thresh_vox_fdr_pstat1.nii.gz')
 
-for samplesize in np.arange(start, 101, 5).tolist():
+    # FWE
+    smoothcmd = 'smoothest -r %s -d %i -m %s' % (
+        res4d, samplesize - 1, mask)
+    smooth = os.popen(smoothcmd).read().split("\n")
+    smoothn = [x.split(' ')[1] for x in smooth[:-1]]
+    reselcount = float(smoothn[1]) / float(smoothn[2])
+    fwethrescmd = 'ptoz 0.05 -g %f' % reselcount
+    fwethres = os.popen(fwethrescmd).read().split("\n")[0]
+    fwecmd = 'fslmaths %s -thr %s zstat1_thresh' % (
+        zstat1, fwethres)
+    os.popen(fwecmd).read()
+    fwe_thres = os.path.abspath('zstat1_thresh.nii.gz')
 
-    np.random.seed(experiment * samplesize)
+    # cluster extent
+    clustercmd = (
+        'cluster -i %s -c %s -t 3.2 -p 0.05 -d %s --volume=%s'
+        ' --othresh=thresh_cluster_fwe_zstat1 --connectivity=26 --mm') % (
+        zstat1, cope1, smoothn[0], smoothn[1])
+    clusterout = os.popen(clustercmd).read()
+    with open('thres_cluster_fwe_table.txt', 'w+') as f1:
+        f1.write(clusterout)
+    return pstat, fdr_thres, fwe_thres
 
-    sampledsubjects = np.random.choice(len(subjects), samplesize * 2, replace=False)
-    sampledsubjects = [np.array(subjects)[sampledsubjects[:samplesize]].tolist(),
-                       np.array(subjects)[sampledsubjects[samplesize:]].tolist()]
 
-    print("##################################################")
-    print("# Started samplesize %i - Sampled subjects:      #" % samplesize)
-    print("##################################################")
-    print(", ".join(sampledsubjects[0]))
-    print(", ".join(sampledsubjects[1]))
-    print("##################################################")
+def main():
+    args = get_parser().parse_args()
 
-    #################
-    ## TEST-RETEST ##
-    #################
+    if args.bids_dir is None:
+        raise RuntimeError('--bids-dir not provided, and $BIDSDIR not set')
 
-    task = args.task
-    contrast = int(args.contrast)
+    # Setup output dir
+    deriv_dir = args.deriv_dir
+    if deriv_dir is None:
+        deriv_dir = os.path.join(args.bids_dir, 'derivatives')
+    base_dir = os.path.join(deriv_dir, args.name)
+    groupdir = os.path.join(base_dir, 'l2-jd')
 
-    # loop over pipelines
+    if not os.path.exists(groupdir):
+        os.makedirs(groupdir)
 
-    for prep_pipeline in ['fmriprep-1.0.3', 'fslfeat_5.0.9']:
+    ##############################
+    # TAKE SAMPLE FOR EXPERIMENT #
+    ##############################
 
-        cf = get_config.get_folders(prep_pipeline)
-        featdir = cf['resdir']
-        groupdir = cf['groupdir']
+    # collect subjects
+    whitelist = os.path.join(
+        os.path.dirname(inspect.getfile(inspect.currentframe())),
+        'utils', 'CNP-stopsignal-whitelist.txt')
 
-        # create folders
-        ssdir = os.path.join(groupdir, "samplesize_%i" % samplesize)
-        outdir = os.path.join(ssdir, "experiment_%i" % experiment)
-        outtaskdir = os.path.join(outdir, task)
+    with open(whitelist) as f:
+        subjects = [s.strip('\n') for s in f.read().splitlines()]
 
-        for directory in [ssdir, outdir, outtaskdir]:
-            if not os.path.exists(directory):
-                os.mkdir(directory)
+    start = 10
+    experiment = args.experiment
+    for samplesize in np.arange(start, 125, 5).tolist():
+        np.random.seed(experiment * samplesize)
 
-        dims = [65, 77, 49] if prep_pipeline.startswith('fmriprep') else [97, 115, 97]
+        sample = np.random.choice(len(subjects), samplesize * 2, replace=False)
+        sample = [sorted(np.array(subjects)[sample[:samplesize]].tolist()),
+                  sorted(np.array(subjects)[sample[samplesize:]].tolist())]
 
-        #######################
-        ## CREATE GROUP MASK ##
-        #######################
+        print("##################################################")
+        print("# Started samplesize %3d - Sampled subjects:    #" % samplesize)
+        print("##################################################")
+        print(", ".join(sample[0]))
+        print(", ".join(sample[1]))
+        print("##################################################")
 
-        groupmaskfile = os.path.join(outtaskdir, "mask.nii.gz")
+        task = args.task
+        contrast = args.contrast
 
-        thesesubjects = [x for sublist in sampledsubjects for x in sublist]
+        # loop over pipelines
+        for pipe in ['fmriprep', 'fslfeat']:
+            for gid in range(2):
+                print("Starting Sample %i" % gid)
+                expname = ('%s_%s_N%03d_R%03d_S%d' % (
+                           pipe, task, samplesize, experiment, gid))
+                destdir = os.path.join(groupdir, expname)
+                if not os.path.exists(destdir):
+                    os.makedirs(destdir)
 
-        mask = np.zeros(dims + [len(thesesubjects)])
-        for k, subject in enumerate(thesesubjects):
-            cf_files = get_config.get_files(prep_pipeline, subject, task)
-            maskfile = cf_files['standard_mask']
-            masksub = nib.load(maskfile)
-            data = masksub.get_data()
-            mask[:, :, :, k] = data
+                group_pattern = [
+                    os.path.join(
+                        base_dir, 'sub-%s' % s, 'func',
+                        'sub-{}_task-{}_variant-{}_%s{:d}.nii.gz'.format(
+                            s, task, pipe, contrast))
+                    for s in sample[gid]
+                ]
 
-        mask = np.mean(mask, axis=3)
-        mask = np.where(mask > 0.8, 1, 0)
-        maskimg = nib.Nifti1Image(mask, affine=masksub.get_affine(), header=masksub.get_header())
-        maskimg.to_filename(groupmaskfile)
+                copes = [gpatt % 'cope' for gpatt in group_pattern]
+                varcopes = [gpatt % 'varcope' for gpatt in group_pattern]
 
-        for sample in range(2):
-            print("Starting Sample %i" % sample)
+                # define nodes
+                copemerge = Node(fsl.Merge(
+                    dimension='t',
+                    in_files=copes),
+                    name='copemerge')
+                varcopemerge = Node(fsl.Merge(
+                    dimension='t',
+                    in_files=varcopes),
+                    name='varcopemerge')
+                level2model = Node(fsl.L2Model(
+                    num_copes=len(copes)),
+                    name='l2model')
+                OLS = Node(fsl.FLAMEO(
+                    run_mode='ols',
+                    mask_file=GROUP_MASK), name='OLS')
 
-            # collect first level analysis folders
+                thres = Node(Function(
+                    function=thresholding,
+                    output_names=['pstat', 'fdr_thres', 'fwe_thres']),
+                    name='thresholds')
+                thres.inputs.mask = GROUP_MASK
+                thres.inputs.samplesize = samplesize
 
-            featdirs = []
-            for sub in sampledsubjects[sample]:
-                subfeatdir = os.path.join(featdir, sub, "%s.feat" % task)
-                if not os.path.exists(subfeatdir):
-                    print("subject not found: %s" % sub)
-                    break
-                else:
-                    featdirs.append(subfeatdir)
+                ds = Node(nio.DataSink(base_directory=groupdir),
+                          name='_'.join(('ds', expname)))
+                dsfmt = ('%s_%s_N%03d_R%03d_S%d.@{}' % (
+                         pipe, task, samplesize, experiment, gid)).format
 
-            # create folders
+                # create workflow
+                CNPgroup = Workflow(name='_'.join(('cnp', expname)))
+                CNPgroup.base_dir = args.work_dir
+                CNPgroup.connect([
+                    (copemerge, OLS, [('merged_file', 'cope_file')]),
+                    (varcopemerge, OLS, [('merged_file', 'var_cope_file')]),
+                    (level2model, OLS, [('design_mat', 'design_file'),
+                                        ('design_con', 't_con_file'),
+                                        ('design_grp', 'cov_split_file')]),
+                    (OLS, ds, [(('tstats', _first), dsfmt('tstat1')),
+                               (('zstats', _first), dsfmt('zstat1'))]),
+                    (OLS, thres, [
+                        (('zstats', _first), 'zstat1'),
+                        (('copes', _first), 'cope1'),
+                        ('res4d', 'res4d')]),
+                    (thres, ds, [
+                        ('pstat', dsfmt('pstat')),
+                        ('fdr_thres', dsfmt('fdr_thres')),
+                        ('fwe_thres', dsfmt('fwe_thres'))]),
+                ])
 
-            sampledir = os.path.join(outtaskdir, "sample_%i" % sample)
-            outcopedir = os.path.join(outtaskdir, "sample_%i" % sample, "cope%i" % (contrast))
-            for directory in [sampledir, outcopedir]:
-                if not os.path.exists(directory):
-                    os.mkdir(directory)
-            copes = [os.path.join(x, 'stats', '%s%i.nii.gz' % ('cope', contrast))
-                     for x in featdirs]
-            varcopes = [os.path.join(x, 'stats', '%s%i.nii.gz' %
-                                     ('varcope', contrast)) for x in featdirs]
+                CNPgroup.write_graph(graph2use='colored')
+                CNPgroup.run('MultiProc', plugin_args={'n_procs': 4})
 
-            # define nodes
+    return 0
 
-            copemerge = Node(interface=fsl.Merge(
-                dimension='t',
-                in_files=copes),
-                name='copemerge')
-            varcopemerge = Node(interface=fsl.Merge(
-                dimension='t',
-                in_files=varcopes),
-                name='varcopemerge')
-            level2model = Node(interface=fsl.L2Model(
-                num_copes=len(copes)),
-                name='l2model')
-            OLS = Node(interface=fsl.FLAMEO(
-                run_mode='ols',
-                mask_file=groupmaskfile),
-                name='OLS')
 
-            # create workflow
+def _first(inlist):
+    if isinstance(inlist, (list, tuple)):
+        return inlist[0]
+    return inlist
 
-            CNPgroup = Workflow(name='cnp_group')
-            CNPgroup.base_dir = outcopedir
-            CNPgroup.connect([(copemerge, OLS, [('merged_file', 'cope_file')]),
-                              (varcopemerge, OLS, [('merged_file', 'var_cope_file')]),
-                              (level2model, OLS, [('design_mat', 'design_file'),
-                                                  ('design_con', 't_con_file'),
-                                                  ('design_grp', 'cov_split_file')]),
-                              ])
 
-            CNPgroup.write_graph(graph2use='colored')
-            CNPgroup.run('MultiProc', plugin_args={'n_procs': 4})
-
-            # remove unwanted files
-            basedir = os.path.join(outcopedir, "cnp_group")
-            resultdir = os.path.join(basedir, 'OLS/stats')
-            destdir = os.path.join(outcopedir, 'OLS')
-            shutil.move(resultdir, destdir)
-
-            shutil.rmtree(basedir)
-
-            ##################
-            ## THRESHOLDING ##
-            ##################
-
-            os.chdir(destdir)
-
-            # FDR
-            os.popen('fslmaths zstat1 -ztop pstat1').read()
-            logpcmd = 'fdr -i pstat1 -m mask -q 0.05'
-            thres = float(os.popen(logpcmd).read().split('\n')[1])
-            threscmd = 'fslmaths pstat1 -mul -1 -add 1 -thr %f -mas mask thresh_vox_fdr_pstat1' % (
-                1 - thres)
-            os.popen(threscmd).read()
-
-            # FWE
-            smoothcmd = 'smoothest -r res4d -d %i -m mask' % (len(featdirs) - 1)
-            smooth = os.popen(smoothcmd).read().split("\n")
-            smoothn = [x.split(' ')[1] for x in smooth[:-1]]
-            reselcount = float(smoothn[1]) / float(smoothn[2])
-            fwethrescmd = 'ptoz 0.05 -g %f' % reselcount
-            fwethres = os.popen(fwethrescmd).read().split("\n")[0]
-            fwecmd = 'fslmaths zstat1 -thr %s thresh_vox_fwe_zstat1' % fwethres
-            fwe = os.popen(fwecmd).read()
-
-            # cluster extent
-            clustercmd = 'cluster -i zstat1 -c cope1 -t 3.2 -p 0.05 -d %s --volume=%s --othresh=thresh_cluster_fwe_zstat1 --connectivity=26 --mm' % (
-                smoothn[0], smoothn[1])
-            clusterout = os.popen(clustercmd).read()
-            f1 = open('thres_cluster_fwe_table.txt', 'w+')
-            f1.write(clusterout)
+if __name__ == '__main__':
+    code = main()
+    sys.exit(code)
--- collect_results.py
+++ collect_results.py
@@ -1,50 +1,36 @@
 #!/usr/bin/env python
 # coding: utf-8
-
 from __future__ import division, absolute_import
 
 import os
+import sys
+import glob
 import numpy as np
 import json
-import progressbar
-import nibabel as nib
+import nibabel as nb
 import pandas as pd
 from scipy.stats import pearsonr
 from nilearn.image import resample_to_img
 from nipype.algorithms.metrics import FuzzyOverlap
 
-from .utils.atlas import create_atlas
-
-pipelines = ['fmriprep-1.0.3', 'fslfeat_5.0.9']
-
-basedir = os.path.join(os.environ.get("PREPBASEDIR"), "fmriprep_vs_feat")
-
+GROUP_MASK = ('/oak/stanford/groups/russpold/data/templates/'
+              'mni_icbm152_nlin_asym_09c/2mm_brainmask.nii.gz')
 
 # Overlap measures
-def fdice(im1, im2):
+def dice(im1, im2, in_mask=None):
     overlap = FuzzyOverlap()
     overlap.inputs.in_ref = [im1]
     overlap.inputs.in_tst = [im2]
-    overlap.inputs.weighting = 'volume'
+
+    if in_mask is not None:
+        overlap.inputs.in_mask = in_mask
     res = overlap.run()
     return res.outputs.dice
 
 
-def dice(im1, im2):
-    binarised = []
-    for sample in [im1, im2]:
-        thres = nib.load(sample).get_data()
-        flt = thres.flatten()
-        binarised.append(np.array([1 if x > 0 else 0 for x in flt]))
-
-    sm = binarised[0] + binarised[1]
-    dice = np.sum(sm == 2) * 2 / np.sum(sm > 0)
-    return dice
-
-
-def cohen(tstat, atlas, labels):
+def cohen(tstat, atlas, labels, samplesize):
     res = {}
-    for k, v in labels.iteritems():
+    for k, v in labels.items():
         indxs = np.where(atlas == k)
         T = tstat[indxs]
         CD = np.mean(T) / np.sqrt(samplesize)
@@ -52,102 +38,104 @@ def cohen(tstat, atlas, labels):
     return res
 
 
-def get_file(samplesize, experiment, basedir, thres='FWE'):
-    out = {}
-    for pipeline in pipelines:
-        out[pipeline] = {}
+def create_atlas():
+    # 1: left STN
+    # 2: right STN
+    STN_05 = os.path.join(os.getenv("FSLDIR"),
+                          'data/atlases/STN/STN-maxprob-thr0-0.5mm.nii.gz')
+    stn_05_2mm = resample_to_img(STN_05, GROUP_MASK,
+                                 interpolation='nearest').get_data()
+    # 4: Inferior Frontal Gyrus, pars triangularis
+    # 25: Juxtapositional Lobule Cortex (formerly Supplementary Motor Cortex)
+    # 7: Precentral gyrus (motor)
+    HO = os.path.join(os.getenv("FSLDIR"),
+                      'data/atlases/HarvardOxford/HarvardOxford-cort-maxprob-thr25-1mm.nii.gz')
+    ho_2mm = resample_to_img(
+        HO, GROUP_MASK, interpolation='nearest').get_data().astype(np.uint8)
 
-        for sample in range(2):
-            prefix = os.path.join(basedir, pipeline, 'task_group/samplesize_%i/experiment_%i' % (samplesize, experiment),
-                                  "stopsignal/sample_%i/cope11/OLS/" % (sample))
-            out[pipeline][sample] = {}
+    atlas_data = np.zeros_like(ho_2mm, dtype=np.uint8)
+    atlas_data[stn_05_2mm > 1] = 1
+    atlas_data[ho_2mm == 26] = 2
+    atlas_data[ho_2mm == 5] = 3
+    atlas_data[ho_2mm == 7] = 4
+    labels = {
+        1: "STN",
+        2: "preSMA",
+        3: "IFG",
+        4: "PCG"
+    }
+    return atlas_data, labels
 
-            thressuf = 'thresh_vox_fwe_tstat1.nii.gz' if thres == 'FWE' else 'thresh_vox_fdr_pstat1.nii.gz'
-            out[pipeline][sample]['thres'] = os.path.join(prefix, thressuf)
-            out[pipeline][sample]['tfile'] = os.path.join(prefix, 'tstat1.nii.gz')
-            out[pipeline][sample]['pfile'] = os.path.join(prefix, 'pstat1.nii.gz')
-
-            if not os.path.exists(out[pipeline][sample]['tfile']):
-                return False
-
-            if False:  # resample to other image, turned off
-
-                thresrssuf = 'thresh_vox_fwe_tstat1_resampled.nii.gz' if thres == 'FWE' else 'thresh_vox_fdr_pstat1_resampled.nii.gz'
-                out[pipeline][sample]['newthres'] = os.path.join(prefix, thresrssuf)
-                out[pipeline][sample]['newtfile'] = os.path.join(prefix, 'tstat1_resampled.nii.gz')
-                out[pipeline][sample]['newpfile'] = os.path.join(prefix, 'pstat1_resampled.nii.gz')
-
-                for pipeline in pipelines:
-                    if pipeline.startswith('fmriprep-1.0.3'):
-                        for idx, kind in enumerate(['tfile', 'pfile', 'thres']):
-                            newkind = ['newtfile', 'newpfile', 'newthres'][idx]
-                            if not os.path.exists(out[pipeline][sample][newkind]):
-                                newimg = resample_to_img(
-                                    out[pipeline][sample][kind], out["fslfeat_5.0.9"][sample][kind], interpolation='nearest')
-                                newimg.to_filename(out[pipeline][sample][newkind])
-                            out[pipeline][sample][kind] = out[pipeline][sample][newkind]
-
-    return out
 
 # COLLECT
+def main():
+    atlas_data, labels = create_atlas()
+    results = pd.DataFrame()
+
+    # Pattern: l2-jd/fslfeat_stopsignal_N120_R100_S0
+    basedir = os.path.join(os.environ.get("PREPBASEDIR"), "fmriprep_vs_feat_2.0-jd", 'l2-jd')
+    l1results = [os.path.basename(f)[:-1] 
+                 for f in sorted(glob.glob(os.path.join(basedir, '*_S0')))]
+    allT = {
+        'fmriprep': {},
+        'fslfeat': {}
+    }
+    for i, res in enumerate(l1results):
+        print('Processing experiment: %s' % res)
+        s1 = os.path.join(basedir, '%s0' % res)
+        s2 = os.path.join(basedir, '%s1' % res)
+
+        pipeline, task, samplesize, exp, _ = res.split('_')
+        samplesize = int(samplesize[1:])
+        exp = int(exp[1:])
+
+        tvals = [
+            nb.load(os.path.join(s1, 'tstat1.nii.gz')).get_data(),
+            nb.load(os.path.join(s2, 'tstat1.nii.gz')).get_data()]
+        # compute mask to have exactly same values
+        tmpmask = np.where(np.logical_and(tvals[0] != 0, tvals[1] != 0))
+
+        tvs = [x[tmpmask].tolist() for x in tvals]
+        tvs = [x for sublist in tvs for x in sublist]
+        sel = np.random.choice(tvs, 100).tolist()
+
+        if ('%d' % samplesize) not in allT[pipeline]:
+            allT[pipeline]['%d' % samplesize] = []
+
+        allT[pipeline]['%d' % samplesize] += sel
+
+        csvrow = {
+            'pipeline': pipeline,
+            'repetition': exp,
+            'N': samplesize,
+        }
+        csvrow.update(cohen(tvals[0], atlas_data, labels, samplesize))
+
+        # compute correlation
+        csvrow["correlation"] = pearsonr(tvals[0][tmpmask], tvals[1][tmpmask])[0]
+
+        pval_names = [os.path.join(s, 'zstat1_pval.nii.gz')
+                      for s in [s1, s2]]
+        pvals = [nb.load(f).get_data() for f in pval_names]
+
+        # compute fuzzy dice
+        csvrow["fdice"] = dice(*pval_names)
+        csvrow["fdice_masked"] = dice(*pval_names, in_mask=GROUP_MASK)
+        
+        binmap = [os.path.join(s, 'zstat1_thresh.nii.gz') for s in [s1, s2]]
+
+        # compute dice
+        csvrow["dice"] = dice(*binmap)
+        csvrow["dice_masked"] = dice(*binmap, in_mask=GROUP_MASK)
+        # Add to Dataframe
+        results = results.append(csvrow, ignore_index=True)
+        print('Added row: %s' % csvrow)
+
+    with open(os.path.join(basedir, "tvals.json"), 'w') as outfile:
+        json.dump(allT, outfile)
+    results.to_csv(os.path.join(basedir, "group.csv"))
+    print('Results file written out to %s.' % os.path.join(basedir, "group.csv"))
 
 
-atlas, labels = create_atlas()
-
-results = pd.DataFrame()
-allT = {}
-
-for pipeline in pipelines:
-    allT[pipeline] = {}
-    k = 0
-    bar = progressbar.ProgressBar()
-    for samplesize in bar(np.arange(10, 101, 5).tolist()):
-        k += 1
-        rng = range(0, 100)
-        allT[pipeline][samplesize] = []
-        for experiment in rng:
-
-            fls = get_file(samplesize, experiment, basedir, thres='FDR')
-            if not fls:
-                continue
-
-            # read in tvals
-            tvals = [nib.load(fls[pipeline][x]['tfile']).get_data() for x in range(2)]
-
-            # compute mask to have exactly same values
-            tmpmask = np.where(np.logical_and(tvals[0] != 0, tvals[1] != 0))
-
-            tvs = [x[tmpmask].tolist() for x in tvals]
-            tvs = [x for sublist in tvs for x in sublist]
-            sel = np.random.choice(tvs, 100).tolist()
-            allT[pipeline][samplesize] += sel
-
-            # prepare atlas
-            if k == 1:
-                atlas_resampled = resample_to_img(
-                    atlas, fls[pipeline][0]['tfile'], interpolation='nearest')
-                dat = atlas_resampled.get_data()
-
-            res = cohen(tvals[0], dat, labels)
-
-            # compute correlation
-            res["correlation"] = pearsonr(tvals[0][tmpmask], tvals[1][tmpmask])[0]
-
-            # compute fuzzy dice
-            res["fdice"] = fdice(fls[pipeline][0]['pfile'], fls[pipeline][1]['pfile'])
-
-            # compute dice
-            res["dice"] = dice(fls[pipeline][0]['thres'], fls[pipeline][1]['thres'])
-
-            res["samplesize"] = samplesize
-            res["pipeline"] = pipeline
-            res["experiment"] = experiment
-
-            results = results.append(res, ignore_index=True)
-
-            del tvals, tmpmask, res, fls
-
-with open(os.path.join(basedir, "tvals.json"), 'w') as outfile:
-    json.dump(allT, outfile)
-
-results.to_csv(os.path.join(basedir, "results.csv"))
+if __name__ == '__main__':
+    main()
